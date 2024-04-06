15c077a55eb53cafe5d3df5d6020f58288e7b63b
legen <legenadary.pigeon@gmail.com>
2024-04-06 18:01:41 +0300

    comments clear

--- TestFilter/LogReadersTests.cs
+++ TestFilter/LogReadersTests.cs
@@ -1,21 +1,20 @@
-﻿using System.Text;
-using IPFilter;
+﻿using IPFilter;
+
+namespace TestFilter;
 
 public class LogReaderTests
 {
     [Fact]
     public async Task ReadLinesAsync_WithRealFile_ReadsCorrectly()
     {
-        // Путь к тестовому файлу
-        var testFilePath = "..\\..\\..\\logfile1.log";
+        const string testFilePath = @"..\..\..\logfile1.log";
 
-        // Создаем тестовый файл
-        File.WriteAllLines(testFilePath, new[] {
+        File.WriteAllLines(testFilePath, [
             "192.168.1.100 2024-01-01T08:15:30+00:00",
             "192.168.1.101 2024-01-02T08:15:30+00:00"
-        });
+        ]);
 
-        var logReader = new LogReader(); // Используем LogReader, который читает из файла
+        var logReader = new LogReader(); 
 
         var lines = new List<string>();
         await foreach (var line in logReader.ReadLinesAsync(testFilePath))
@@ -23,12 +22,10 @@ public class LogReaderTests
             lines.Add(line);
         }
 
-        // Проверяем результаты
-        Assert.Equal(2, lines.Count); // убедимся, что прочитаны обе строки
+        Assert.Equal(2, lines.Count); 
         Assert.Equal("192.168.1.100 2024-01-01T08:15:30+00:00", lines[0]);
         Assert.Equal("192.168.1.101 2024-01-02T08:15:30+00:00", lines[1]);
 
-        // Удаляем тестовый файл после выполнения теста
         File.Delete(testFilePath);
     }
 }
